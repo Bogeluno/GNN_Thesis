93bedbce41cb956c5b6ac71f073da50962f25af9
Bogeluno <bogeluno@gmail.com>
2021-11-23 09:01:45 +0100

    T morning

--- models/MakeSimpleData.py
+++ models/MakeSimpleData.py
@@ -6,13 +6,19 @@ from tqdm import tqdm
 
 
 # Get trafic index
-tdf = pd.read_csv('data/processed/Vacancy_new.csv', parse_dates=[2,3])
-tdf['park_time'] = tdf.park_time.round('h').dt.hour
-trafic_index = dict(tdf.groupby('park_time').mean()['time_to_reservation'])
-del tdf
+#tdf = pd.read_csv('data/processed/Vacancy_new.csv', parse_dates=[2,3])
+#tdf['park_time'] = tdf.park_time.round('h').dt.hour
+#trafic_index = dict(tdf.groupby('park_time').mean()['time_to_reservation'])
+#del tdf
+
+
 
 # Load data 
 df = pd.read_csv('data/processed/Fall2019P.csv', index_col=0, parse_dates=[1])
+
+df['timeh'] = df.time.round('h').dt.hour
+trafic_index = dict(df.groupby('timeh').mean()['time_to_reservation'])
+df.drop(columns = ['timeh'], inplace=True)
 
 # Only to customers
 df = df[df.next_customer]
@@ -20,14 +26,23 @@ df.drop(columns=['prev_customer', 'next_customer'], inplace = True)
 
 # Time variables
 df['weekend'] = df.time.dt.weekday//5
+
+def circle_transform(col, max_val=86400):
+    tot_sec = ((col - col.dt.normalize()) / pd.Timedelta('1 second')).astype(int)
+    cos_val = np.cos(2*np.pi*tot_sec/max_val)
+    sin_val = np.sin(2*np.pi*tot_sec/max_val)
+    return cos_val, sin_val
+
+df['Time_Cos'], df['Time_Sin'] = [x.values for x in circle_transform(df.time)]
 df['hour_index'] = df.time.dt.hour.map(trafic_index)
 
-# No more than 3 days
+# No more than 2 days
 df = df[df.time_to_reservation < 48]
 
 # Remove zones with too little support
 df = df[~df.leave_zone.isin((df.leave_zone.value_counts() < 30).index[df.leave_zone.value_counts() < 30])]
 
+# One hot encoding
 df = pd.get_dummies(df, columns = ['engine','leave_zone'], prefix=['eng','lz'])
 
 # Join weather
@@ -69,7 +84,7 @@ def haversine(point1, point2):
     # Deltas
     delta_lon = lon2 - lon1 
     delta_lat = lat2 - lat1 
-        
+
     # haversine formula 
     a = np.sin(delta_lat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(delta_lon/2)**2
     c = 2 * np.arcsin(np.sqrt(a)) 
@@ -82,7 +97,7 @@ df['dist_to_station'] = [min({k:haversine(v,r[1].values) for k,v in Stations.ite
 df = df[df['dist_to_station'] <= 7000]
 
 # Drop some columns
-df.drop(columns=['index','park_location_lat', 'park_location_long', 'leave_location_lat', 'leave_location_long', 'leave_fuel', 'park_zone', 'moved', 'movedTF', 'park_time', 'reserve_time'], inplace = True)
+df.drop(columns=['park_location_lat', 'park_location_long', 'leave_location_lat', 'leave_location_long', 'park_fuel', 'park_zone', 'moved', 'movedTF', 'park_time', 'reserve_time'], inplace = True)
 
 # Save
 df.to_csv('data/processed/SimpleNNData.csv')
